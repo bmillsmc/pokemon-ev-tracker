57a933b21de0c606b0b8c9d1bb3f6ad9dd073a4e
Ben Miller <benbmiller123@gmail.com>
2019-11-30 17:45:39 -0500

    added example, not working

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from peewee import *
 from datetime import date
 # run this with pipenv run python3 main.py or using python3 in the pipenv shell
 
-db = PostgresqlDatabase("pokemon", user="postgres", password="", host="localhost", port="8080")
+db = PostgresqlDatabase("pokemon", user="postgres", password="", host="localhost", port="5432")
 
 class BaseModel(Model):
     class Meta:
@@ -18,3 +18,11 @@ class Pokemon(BaseModel):
     spd = IntegerField() # special defense ev stat
     hp = IntegerField() # health points ev stat
     total_evs = IntegerField() # how many evs the pokemon has gained, shouldn't exceed 512
+
+db.connect()
+db.drop_tables([Pokemon])
+db.create_tables([Pokemon])
+
+aegislash = Pokemon(name="firstaegislash", species="aegislash", atk=0, defen=0, spe=0, spa=0, spd=0, hp=0, total_evs=0)
+aegislash.save()
+print(f"{aegislash.id}: {aegislash.species} (\"{aegislash.name}\") - EVs: {aegislash.total_evs}")
