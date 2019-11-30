e385da4e0430e9514e99066eaf03328613972018
Ben Miller <benbmiller123@gmail.com>
2019-11-30 17:38:10 -0500

    added db connection and created pokemon model

---
+++ main.py
@@ -0,0 +1,20 @@
+from peewee import *
+from datetime import date
+# run this with pipenv run python3 main.py or using python3 in the pipenv shell
+
+db = PostgresqlDatabase("pokemon", user="postgres", password="", host="localhost", port="8080")
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Pokemon(BaseModel):
+    name = TextField(unique=True) # identifier for pokemon, for example, kingsshieldaegislash
+    species = TextField() # pokemon species, for example, aegislash
+    atk = IntegerField() # attack ev stat
+    defen = IntegerField() # defense ev stat
+    spe = IntegerField() # speed ev stat
+    spa = IntegerField() # special attack ev stat
+    spd = IntegerField() # special defense ev stat
+    hp = IntegerField() # health points ev stat
+    total_evs = IntegerField() # how many evs the pokemon has gained, shouldn't exceed 512
